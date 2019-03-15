a72b26e318e4a4e68f15e01942cee004312ba1c6
Administrator <15667078861@163.com>
2019-03-15 23:21:21 +0800

    字符串方法练习

--- src/main/java/Demo1.java
+++ src/main/java/Demo1.java
@@ -12,6 +12,9 @@ public class Demo1 {
         System.out.println(s1.equals(s2+s3));
         readString1(s1);
         readString2(s2);
+        int2String(100);
+        demo1("rEPLACE");
+        demo2("woaijava buguanshijava1haishijava2 doushihaojava", "java");
     }
 
     public static void readString1(String input){
@@ -26,6 +29,30 @@ public class Demo1 {
         for (char i:arry) {
             System.out.println(i);
         }
+    }
+
+    public static void int2String(int a){
+        System.out.println(String.valueOf(a));
+    }
+
+    public static void demo1(String a){
+        String s1 = a.substring(0,1).toUpperCase();
+        String strRemaining = a.substring(1).toLowerCase();
+        System.out.println(s1 + strRemaining);
+    }
+
+    public static void demo2(String momStr, String subStr){
+        int cnt = 0;
+        while (true){
+            int flagCode = momStr.indexOf(subStr);
+            if (-1==flagCode){
+                break;
+            }else {
+             momStr = momStr.substring(flagCode + subStr.length());
+             cnt++;
+            }
+        }
+        System.out.println(cnt);
     }
 
 }
