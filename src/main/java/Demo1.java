71d15fefc377f223ff763126eb17ebf0aa8f7b20
Administrator <15667078861@163.com>
2019-03-15 22:24:36 +0800

    遍历字符串

--- src/main/java/Demo1.java
+++ src/main/java/Demo1.java
@@ -10,5 +10,22 @@ public class Demo1 {
         String s3 = "c";
         System.out.println(s1==s2 + s3);
         System.out.println(s1.equals(s2+s3));
+        readString1(s1);
+        readString2(s2);
     }
+
+    public static void readString1(String input){
+        for (int i=0; i<input.length(); i++){
+            System.out.println(input.charAt(i));
+        }
+    }
+
+    public static void readString2(String input){
+        char[]  arry = input.toCharArray();
+        System.out.println(arry.length);
+        for (char i:arry) {
+            System.out.println(i);
+        }
+    }
+
 }
