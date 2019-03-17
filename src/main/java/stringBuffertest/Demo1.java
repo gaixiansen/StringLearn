554a0b7c8ae3d6d4dde4580c104e8166db9d2eb2
Administrator <15667078861@163.com>
2019-03-17 20:26:10 +0800

    StringBuffer test

---
+++ src/main/java/stringBuffertest/Demo1.java
@@ -0,0 +1,20 @@
+package stringBuffertest;
+import java.lang.StringBuffer;
+/**
+ * @ Author: 瞎驴
+ * @ Date: 2019/3/17 20:14
+ * @ Version 1.0
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        System.out.println(sb.length());
+        System.out.println(sb.capacity());
+        System.out.println(sb.toString());
+        sb.append("hello");
+        System.out.println(sb.toString());
+        System.out.println(sb.capacity());
+        sb.insert(5, " world");
+        System.out.println(sb.toString());
+    }
+}
