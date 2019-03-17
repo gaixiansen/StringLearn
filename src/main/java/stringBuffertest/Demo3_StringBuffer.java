745973dc3ff4630942595b0ec055111391321a7a
Administrator <15667078861@163.com>
2019-03-17 21:40:01 +0800

    StringBuffer test

---
+++ src/main/java/stringBuffertest/Demo3_StringBuffer.java
@@ -0,0 +1,24 @@
+package stringBuffertest;
+
+import java.util.Scanner;
+
+/**
+ * @ Author: 瞎驴
+ * @ Date: 2019/3/17 21:17
+ * @ Version 1.0
+ */
+public class Demo3_StringBuffer {
+    public static void main(String[] args) {
+        System.out.println("please input a String:");
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = strReverse(s1);
+        System.out.println(s2);
+    }
+
+    public static String strReverse(String line){
+        StringBuffer sb = new StringBuffer(line);
+        sb.reverse();
+        return sb.toString();
+    }
+}
