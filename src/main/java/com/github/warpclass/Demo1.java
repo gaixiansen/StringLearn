3c3f12eafab47c2cf22034ffe222992ce05c7d66
Administrator <15667078861@163.com>
2019-03-18 21:53:33 +0800

    基础数据类型封装类

---
+++ src/main/java/com/github/warpclass/Demo1.java
@@ -0,0 +1,16 @@
+package com.github.warpclass;
+
+/**
+ * @ Author: 瞎驴
+ * @ Date: 2019/3/18 20:55
+ * @ Version 1.0
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        String a = Integer.toBinaryString(60);
+        System.out.println("60=" + a);
+        Byte b = Byte.parseByte("101", 2);
+        System.out.println(b);
+
+    }
+}
