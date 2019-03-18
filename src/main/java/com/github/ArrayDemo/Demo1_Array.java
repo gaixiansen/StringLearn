c9840c11f50d6e8d471a6d9126f8002ddfdce98f
Administrator <15667078861@163.com>
2019-03-18 20:39:17 +0800

    Arrays test

---
+++ src/main/java/com/github/ArrayDemo/Demo1_Array.java
@@ -0,0 +1,21 @@
+package com.github.ArrayDemo;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * @ Author: 瞎驴
+ * @ Date: 2019/3/18 20:33
+ * @ Version 1.0
+ */
+public class Demo1_Array {
+
+    public static void main(String[] args) {
+        int[] a = {11,33,22,55,44};
+        System.out.println(Arrays.toString(a));
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+        int index = Arrays.binarySearch(a, 55);
+        System.out.println("55=" + index);
+    }
+}
