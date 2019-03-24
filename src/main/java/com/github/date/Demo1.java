2c31e5e73be5514d8110679f1754d29b01bd9c33
geshunxin <geshunxin@163.com>
2019-03-24 12:11:40 +0800

    Demo date

---
+++ src/main/java/com/github/date/Demo1.java
@@ -0,0 +1,31 @@
+package com.github.date;
+
+import java.lang.String;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Demo1 {
+	/**
+	 * 时间显示，以及时间格式化
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Date d = new Date();
+		System.out.println(d);
+		System.out.println(d.getTime());
+		System.out.println(System.currentTimeMillis());
+		System.out.println("------------------------");
+		SimpleDateFormat sd = new SimpleDateFormat();
+		System.out.println(sd.format(d));
+		System.out.println("-------------------------");
+		SimpleDateFormat sd2 = new SimpleDateFormat("Y/M/d H:m:s");
+		System.out.println(sd2.format(d));
+		String s = "2019/08/08 19:32:34";
+		try {
+			System.out.println(sd2.parse(s));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+}
