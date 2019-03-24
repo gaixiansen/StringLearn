51373fb60bff926867a371107518f05fe4af8dff
geshunxin <geshunxin@163.com>
2019-03-24 16:25:09 +0800

    Demo date

--- src/main/java/com/github/date/Demo1.java
+++ src/main/java/com/github/date/Demo1.java
@@ -10,7 +10,7 @@ public class Demo1 {
 	 * 时间显示，以及时间格式化
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Date d = new Date();
 		System.out.println(d);
 		System.out.println(d.getTime());
@@ -27,5 +27,17 @@ public class Demo1 {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+		getdays();
+
+	}
+	public  static void getdays() throws ParseException {
+		String birthday = "1992/06/09";
+		Date d = new Date();
+		SimpleDateFormat sd = new SimpleDateFormat("Y/M/d");
+		String today = sd.format(d);
+		System.out.println(today);
+		long birthdayDay = sd.parse(birthday).getTime();
+		long myDay =  (sd.parse(today).getTime() - birthdayDay)/1000/60/60/24;
+		System.out.println("我已经活了"+ myDay + "天");
 	}
 }
