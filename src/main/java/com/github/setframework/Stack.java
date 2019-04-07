ca835dcce17e902c868b71f392f84fb8d69534c0
geshunxin <geshunxin@163.com>
2019-04-07 14:22:31 +0800

    利用LinkedList 创建栈数据模型

---
+++ src/main/java/com/github/setframework/Stack.java
@@ -0,0 +1,36 @@
+package com.github.setframework;
+
+import org.omg.CORBA.OBJ_ADAPTER;
+
+import java.util.LinkedList;
+
+/**
+ * @ProjectName: JavaLearn
+ * @Package: com.github.setframework
+ * @ClassName: Stack
+ * @Description: 封装LinkedList，写一个类似栈的数据结构
+ * @Author:
+ * @CreateDate: 2019/4/7 11:37
+ * @UpdateUser:
+ * @UpdateDate: 2019/4/7 11:37
+ * @UpdateRemark:
+ * @Version: 1.0
+ */
+public class Stack {
+	private LinkedList<String> list = new LinkedList<String>();
+
+	public LinkedList<String> Stack(){
+		return this.list;
+	}
+	public void in(String obj){
+		list.addLast(obj);
+	}
+
+	public Object out(){
+		return list.removeLast();
+	}
+
+	public boolean isEmpty(){
+		return list.isEmpty();
+	}
+}
